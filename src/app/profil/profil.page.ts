baa81519cf384998f3d58d58f7e100dfcc8fcf1a
Alexandre Verhoye <alexandreverhoye@gmail.com>
2018-11-16 20:17:37 +0100

    Changement padding + Ajout alerte de deconnexion

--- src/app/profil/profil.page.ts
+++ src/app/profil/profil.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-profil',
@@ -6,4 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['profil.page.scss']
 })
 
-export class ProfilPage {}
+export class ProfilPage {
+
+  constructor(public alertCtrl: AlertController) {}
+
+  async decoAlert() {
+    
+        const alert = await this.alertCtrl.create({
+          header: 'Se déconnecter ?',
+          message: 'Vous pouvez vous reconnecter a tout moment dans cet onglet',
+          buttons: [
+            {
+              text: 'Annuler',
+              role: 'cancel',
+              cssClass: 'secondary',
+              handler: (blah) => {
+                console.log('Confirm Cancel: blah'); //Evenement lors de l'annulation
+              }
+            }, 
+            
+            {
+              text: 'Déconnexion',
+              handler: () => {
+                console.log('Confirm Okay'); //Evenement lors de la deconnexion
+              }
+            }
+          ]
+        });
+  
+      await alert.present();
+    }
+  
+
+  
+  doConfirm() {
+  }
+  doPrompt() {
+  }
+ doRadio() {
+  }
+ doCheckbox() {
+  }
+}
+
+
