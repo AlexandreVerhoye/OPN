2a3c99fc65bd60b602029f1c0afb89cd502b68a5
Alexandre Verhoye <alexandreverhoye@gmail.com>
2018-11-20 10:59:13 +0100

    Ajout de fonction  getNom() pour accueil + toast pour fonction non disponible

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss']
 })
-export class HomePage {}
+export class HomePage {
+
+  constructor(public toastController: ToastController) {}
+
+  public getNom() : string{ //Retourne le prenom et nom de la personne connectée
+    var nom = "Alexandre Verhoye"; //(a titre d'exemple en attendant le script)
+    return nom;
+  }
+
+  async fonctionNonDisponible() {
+    const toast = await this.toastController.create({
+      message: 'Pas disponible pour le moment.',
+      duration: 10000,
+      position: 'top',
+      showCloseButton: true,
+      closeButtonText: 'Ok'
+    });
+    toast.present();
+  }
+}
