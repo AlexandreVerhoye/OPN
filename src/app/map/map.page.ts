15128ed0791ca25bbc9d455bb1dd8b618677e24f
Alexandre Verhoye <alexandreverhoye@outlook.com>
2018-11-21 17:09:50 +0100

    Localisation de la map (sur pc)

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -19,9 +19,16 @@ export class MapPage {
   loadmap() {
     this.map = leaflet.map("map").fitWorld();
     leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attributions: 'www.tphangout.com',
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
     }).addTo(this.map);
+    this.map.locate({
+      setView: true,
+      maxZoom: 10
+    }).on('locationfound', (e) => {
+      console.log('found you');
+      })
+ 
   }
 
 }
