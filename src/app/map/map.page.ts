1c6d9a3b31ac4fd86a0121deedeb4d4210f601b3
Alexandre Verhoye <alexandreverhoye@outlook.com>
2018-11-21 16:58:50 +0100

    Ajout du module leafleet + ajout de la carte

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import leaflet from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: 'map.page.html',
   styleUrls: ['map.page.scss']
 })
-export class MapPage {}
+export class MapPage {
+	
+	map: any;
+	
+	constructor() {}
+	
+	ionViewDidEnter() {
+    this.loadmap();
+  }
+
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'www.tphangout.com',
+      maxZoom: 18
+    }).addTo(this.map);
+  }
+
+}
