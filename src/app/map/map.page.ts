0c423c3e3328d734191d08dd6fdc30cdbf350b9d
Alexandre Verhoye <alexandreverhoye@outlook.com>
2018-11-22 11:18:42 +0100

    Geolocation working on Android

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import leaflet from 'leaflet';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 
 @Component({
@@ -10,22 +11,26 @@ import leaflet from 'leaflet';
 export class MapPage {
 	map: any;
 	
-	constructor() {}
+	constructor(private geolocation: Geolocation) {}
 
 	ionViewDidEnter() {
     setTimeout(this.loadmap.bind(this), 1000)
   }
+  
 
   async loadmap() {
+	  
     this.map = leaflet.map("map").fitWorld();
 
     leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
       maxZoom: 18
     }).addTo(this.map);
-    this.map.locate({
+    
+	this.map.locate({
       setView: true,
-      maxZoom: 15 //Zoom de 15 (assez proche)
+      maxZoom: 15, //Zoom de 15 (assez proche)
+	  enableHighAccuracy : true
     }).on('locationfound', (e) => {
       console.log('found you');
       })
