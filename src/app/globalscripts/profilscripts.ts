d145b089ab39aea576d7a6845dec52611ee2db9a
Alexandre Verhoye <alexandreverhoye@outlook.com>
2018-12-12 11:04:33 +0100

    Ajout de loadProfil()

--- src/app/globalscripts/profilscripts.ts
+++ src/app/globalscripts/profilscripts.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { globalscripts } from '../globalscripts/globalscripts'
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Component } from '@angular/core';
 
 @Injectable()
 export class profilscripts {
 
-constructor(private gs : globalscripts) {}
+  public items: any;
+  public nom;
+
+constructor(private gs : globalscripts, private http : HttpClient) {}
 
   /*Function getNom() qui retourne le nom de l'utilisateur */
   public getNom() : string{
@@ -27,12 +33,31 @@ constructor(private gs : globalscripts) {}
   }
   */
 
+  
   /*Function deconnexion() correspond au script lorsque l'utilisateur souhaite se deconnecter */
   public deconnexion(){
     console.log('Script deconnexion : en cours');
     //code pour deconnexion
     this.gs.toastBasic('Deconnecté', 1000);
     console.log('Script deconnexion : succès');
+  }
+
+
+  /*Function loadProfil() qui permet de recuperer le profil de l'utilisateur*/
+  /*En cours de dev*/
+  loadProfil(){
+    let data:Observable<any>;
+    data = this.http.get('http://localhost/testOPN/retrieve-data.php')
+    data.subscribe(result =>{
+      this.items = result;
+      let json = JSON.stringify(this.items); 
+      let re = /\[/gi;
+      json = json.replace(re, '');
+      re = /\]/gi;
+      json = json.replace(re, '');
+      let res = JSON.parse(json);
+      this.nom = res.nom;
+    })
   }
 
   
