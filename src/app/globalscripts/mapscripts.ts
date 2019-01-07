5b5246b8d1a7d66b014a47df6273b37f55a9fb69
Alexandre Verhoye <alexandreverhoye@gmail.com>
2019-01-07 21:28:19 +0100

    Mapscripts

--- src/app/globalscripts/mapscripts.ts
+++ src/app/globalscripts/mapscripts.ts
@@ -44,15 +44,6 @@ export class mapscripts {
 
   public navigateTo(x : number, y : number){
     let destination = x + ',' + y;
-
-    if(this.platform.is('ios')){
-      window.open('maps://?q=' + destination, '_system');
-    } else {
-      let label = encodeURI('My Label');
-      window.open('geo:0,0?q=' + destination + '(' + label + ')', '_system');
-    }
-      }
-
       
 
 
@@ -60,3 +51,4 @@ export class mapscripts {
 
   }
       
+}
